0c8bbf4352a9e3ae3240cc23706c78822f736054
unknown <kathpaliadhruv@gmail.com>
2020-08-01 21:41:49 +0530

    Changed width of button and helper text

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './login.css';
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiFormHelperText: {
+        contained: {
+            marginLeft: 0,
+            marginRight: 0
+        },
+      },
+    },
+});
+          
 
 const Login = () => {
     return ( 
-        <div className='login-form'>
-            <form>
-                <h2>
-                    Welcome Back
-                </h2>
-                <TextField
-                    className='form-input'
-                    id="outlined-helperText"
-                    label="Enter your Mobile Number"
-                    helperText='By clicking on "Send OTP" button, I agree to receive OTP for Mobile Number verification.'
-                    variant="outlined"
-                /><br/>
-                <button>Send OTP</button>
-            </form>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className='login-form'>
+                <form>
+                    <h2>
+                        Welcome Back
+                    </h2>
+                    <TextField
+                        className='form-input'
+                        id="outlined-helperText"
+                        label="Enter your Mobile Number"
+                        helperText='By clicking on "Send OTP" button, I agree to receive OTP for Mobile Number verification.'
+                        variant="outlined"
+                    /><br/>
+                    <button>Send OTP</button>
+                </form>
+            </div>
+        </ThemeProvider>
      );
 }
  
